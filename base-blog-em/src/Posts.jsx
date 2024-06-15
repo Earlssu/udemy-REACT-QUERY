faf2c18307268c3815a6e389bee901c96fe06c1a
minseobshim <mshim@blast-team.com>
2024-06-15 16:20:25 +0900

    lecture 1-8: staleTime vs gcTime

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,40 +1,64 @@
-import { useState } from "react";
+import {useState} from "react";
+import {PostDetail} from "./PostDetail";
+import {useQuery} from "@tanstack/react-query";
+import {fetchPosts} from "./api.js";
 
-import { fetchPosts, deletePost, updatePost } from "./api";
-import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
-  const [selectedPost, setSelectedPost] = useState(null);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const data = [];
+    const {data, isError, error, isLoading} = useQuery({
+        queryKey: ["posts"],
+        queryFn: fetchPosts,
+        staleTime: 2000,
+    });
 
-  return (
-    <>
-      <ul>
-        {data.map((post) => (
-          <li
-            key={post.id}
-            className="post-title"
-            onClick={() => setSelectedPost(post)}
-          >
-            {post.title}
-          </li>
-        ))}
-      </ul>
-      <div className="pages">
-        <button disabled onClick={() => {}}>
-          Previous page
-        </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
-          Next page
-        </button>
-      </div>
-      <hr />
-      {selectedPost && <PostDetail post={selectedPost} />}
-    </>
-  );
+    if (isLoading) {
+        return (
+            <div>
+                <h3>Loading . . .</h3>
+            </div>
+        )
+    }
+
+    if (isError) {
+        return (
+            <div>
+                <h3>Oops! something went wrong</h3>
+                <p>{error.toString()}</p>
+            </div>
+        )
+    }
+
+
+    return (
+        <>
+            <ul>
+                {data.map((post) => (
+                    <li
+                        key={post.id}
+                        className="post-title"
+                        onClick={() => setSelectedPost(post)}
+                    >
+                        {post.title}
+                    </li>
+                ))}
+            </ul>
+            <div className="pages">
+                <button disabled onClick={() => {
+                }}>
+                    Previous page
+                </button>
+                <span>Page {currentPage + 1}</span>
+                <button disabled onClick={() => {
+                }}>
+                    Next page
+                </button>
+            </div>
+            <hr/>
+            {selectedPost && <PostDetail post={selectedPost}/>}
+        </>
+    );
 }
