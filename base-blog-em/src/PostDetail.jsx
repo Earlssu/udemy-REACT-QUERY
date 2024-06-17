a5965a82b9b7ee74042552096247c3bd0f9f1ce8
minseobshim <mshim@blast-team.com>
2024-06-17 15:35:43 +0900

    lecture 1-11: quiz

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,21 +1,45 @@
-import { fetchComments } from "./api";
 import "./PostDetail.css";
+import {useQuery} from "@tanstack/react-query";
+import {fetchComments} from "./api.js";
 
-export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+export function PostDetail({post}) {
 
-  return (
-    <>
-      <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
-      <p>{post.body}</p>
-      <h4>Comments</h4>
-      {data.map((comment) => (
-        <li key={comment.id}>
-          {comment.email}: {comment.body}
-        </li>
-      ))}
-    </>
-  );
+    const {data, isError, isLoading, error} = useQuery(
+        {
+            queryKey: ["postComments", post.id],
+            queryFn: () => fetchComments(post.id)
+        }
+    )
+
+    if (isLoading) {
+        return (
+            <div>
+                <h3>Loading . . .</h3>
+            </div>
+        )
+    }
+
+    if (isError) {
+        return (
+            <div>
+                <h3>Oops! something went wrong</h3>
+                <p>{error.toString()}</p>
+            </div>
+        )
+    }
+
+    return (
+        <>
+            <h3 style={{color: "blue"}}>{post.title}</h3>
+            <button>Delete</button>
+            <button>Update title</button>
+            <p>{post.body}</p>
+            <h4>Comments</h4>
+            {data?.map((comment) => (
+                <li key={comment.id}>
+                    {comment.email}: {comment.body}
+                </li>
+            ))}
+        </>
+    );
 }
